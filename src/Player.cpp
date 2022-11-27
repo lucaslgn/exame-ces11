b6b077ac349fa8a3eb3a4cb4c4ca2e3b55be2766
Lucas <nevesguimaraeslucas@gmail.com>
2022-11-27 11:33:26 -0300

    Adding draw of character and walk function

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,11 +1,30 @@
 #include "Player.hpp"
 
-void Player::walk(Map gameMap) {
+void Player::draw(sf::RenderTarget &target, sf::RenderStates states) const {
+	target.draw(player_, states);
+}
+bool Player::readPlayer(const std::string& saveFile, const std::string& imageFile, const Map& gameMap) {
+	std::ifstream is(saveFile);
+	int x, y;
+	if (!(is >> x >> y))
+		return false;
+	auto index = gameMap.closestTile(Position{x, y});
+	pos = gameMap.tilePosition(index);
+
+	if (!playerImage_.loadFromFile(imageFile))
+		return false;
+	player_.setTexture(playerImage_);
+	player_.setPosition(pos.x, pos.y);
+	player_.setOrigin();
+	return true;
+}
+void Player::walk(const Map& gameMap) {
 	auto currentVertex = gameMap.closestTile(pos);
 	auto destinyVertex = gameMap.closestTile(destiny);
 	auto shortestPath = gameMap.shortestPath(currentVertex, destinyVertex);
 
 	for (const auto& vertex : shortestPath) {
 		pos = gameMap.tilePosition(vertex);
+		player_.setPosition(pos.x, pos.y);
 	}
 }
--- src/Player.hpp
+++ src/Player.hpp
@@ -1,10 +1,18 @@
 #pragma once
 
 #include "Map.hpp"
+#include <SFML/Graphics/Drawable.hpp>
+#include <fstream>
 
-class Player {
+class Player : public sf::Drawable {
+private:
+	sf::Texture playerImage_;
+	sf::Sprite player_;
+
+    void draw(sf::RenderTarget&, sf::RenderStates) const override;
 public:
-	void walk(Map);
+	bool readPlayer(const std::string&, const std::string&, const Map&);
+	void walk(const Map&);
 	Position pos;
 	Position destiny;
 };
